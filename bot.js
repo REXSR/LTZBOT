c51665c941fbaad3d6bec373c30466866f47bc77
REXSR <41483292+REXSR@users.noreply.github.com>
2018-09-27 00:30:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -624,9 +624,6 @@ client.on('voiceStateUpdate', (voiceOld, voiceNew) => {
 const fs = require("fs"); //npm i fs
 const Canvas = require("canvas");//for linux = npm i canvas | for windows = npm i canvas-prebuilt
 const jimp = require("jimp");// npm i jimp
-const prefix = "="; // prefix
-const Discord = require('discord.js');// npm i discord.js
-const client = new Discord.Client();
 const id = JSON.parse(fs.readFileSync("./id/mozo.json", "utf8"));
 client.on("message", message => {
   if (message.author.bot) return;
