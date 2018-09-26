77c3b073b53648d97040ccf3906ff49e4bce4618
REXSR <41483292+REXSR@users.noreply.github.com>
2018-09-26 17:48:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-
+const perfix = "!";
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -10,7 +10,7 @@ client.on('ready', () => {
 
     console.log(`[NAWAF] ${client.users.size}`)
 
-    client.user.setStatus("online")
+    client.user.setStatus("ldle")
 
 });
 
