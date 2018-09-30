bd832fd6f1e8777be7a7919ae0bfd4f2363a10f4
REXSR <41483292+REXSR@users.noreply.github.com>
2018-10-01 01:21:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -644,6 +644,8 @@ client.on('message', async message => {
 
 client.on('message', message => {
   if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
   let command = message.content.split(" ")[0];
   command = command.slice(prefix.length);
 
