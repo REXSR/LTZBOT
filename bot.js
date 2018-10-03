04759a12b45e2306d79f49898df47ace28ce04de
REXSR <41483292+REXSR@users.noreply.github.com>
2018-10-03 05:25:02 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -664,30 +664,86 @@ message.channel.send(args.join("  "))
           
 
 client.on('message', message => {
-   if (message.content.startsWith("!id")) {
-                if(!message.channel.guild) return message.reply('**هذا الامر فقط في السيرفرات وشكرا**');
 
-               var mentionned = message.mentions.users.first();
-    var mentionavatar;
-      if(mentionned){
-          var mentionavatar = mentionned;
-      } else {
-          var mentionavatar = message.author;
-          
-      }
-   let embed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-   .setThumbnail(`${mentionavatar.avatarURL}`)
-  .addField("الاسم:",`<@` + `${mentionavatar.id}` + `>`, true)
-   .addField("ايديك:", "**[" + `${Sal.author.id}` + "]**", true)
-   .addField("تاقك:", "**[" + `${Sal.author.discriminator}` + "]**", true)
-  .addField("تم الانشاء في:", "**[" + `${mentionavatar.createdAt}` + "]**", true)
- .addField("تاريخ دخولك للسيرفر:", ", "**[" + `${sal.message.member.joinedAt.toLocaleString}` + "]**", true)   
-     
-  message.channel.sendEmbed(embed);
-  console.log("[id] Send By: " + message.author.username)
+              if(!message.channel.guild) return;
+
+    var prefix = "!";
+
+    if(message.content.startsWith('!bc')) {
+
+    if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+
+  if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+
+    let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+
+
+    let request = `Requested By ${message.author.username}`;
+
+    if (!args) return message.reply('**اكتب شي لي ارسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
+
+    msg.react('✅')
+
+    .then(() => msg.react('❌'))
+
+    .then(() =>msg.react('✅'))
+
+    let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+
+    let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+       let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+
+    let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+
+    reaction1.on("collect", r => {
+
+    message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
+
+    message.guild.members.forEach(m => {
+
+    var bc = new
+
+       Discord.RichEmbed()
+
+       .setColor('RANDOM')
+
+       .setTitle('Broadcast')
+
+       .addField('Server', message.guild.name)
+
+       .addField('Sender', message.author.username)
+
+       .addField('Message', args)
+
+       .setThumbnail(message.author.avatarURL)
+
+       .setFooter(copy, client.user.avatarURL);
+
+    m.send({ embed: bc })
+
+    msg.delete();
+
+    })
+
+    })
+
+    reaction2.on("collect", r => {
+
+    message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+
+    msg.delete();
+
+    })
+
+    })
+
     }
-});
+
+    });  
+  
+ 
+ 
 
 
  
